48912a009ea04eddebba832bd65e242e8a5ef1ca
emanuiler <emanuil.mihaylov@gmail.com>
2018-03-20 16:06:55 +0200

    Basic Pong game 2d

---
+++ Pong/Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour {
+
+    public float speed = 30;
+
+    private void FixedUpdate()
+    {
+        float verticalPress = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, verticalPress) * speed;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
